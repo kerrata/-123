f6e81a2b1a13d63e83d5467b5bcea5801f044a1b
kerrata <144777104+kerrata@users.noreply.github.com>
2025-02-26 08:06:15 +0500

    Add files via upload

---
+++ лаба4/лаба5/Услуги.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛБ5
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string Name { get; set; }
+
+        public Service(int serviceId, string name)
+        {
+            ServiceId = serviceId;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ServiceId}, Название: {Name}";
+        }
+    }
+}
